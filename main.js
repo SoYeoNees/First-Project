c1cae3657d027376e74de214dc226bb093ef7e75
SoYeon Park <162876716+SoYeoNees@users.noreply.github.com>
2024-08-25 22:57:28 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+  }
+
+
+  // script.js
+
+// const texts = ["The Best Rewards.", "The Best Performance."];
+// let currentIndex = 0;
+// const textElement = document.getElementsById('active');
+
+// function typeText() {
+//     textElement.textContent = texts[currentIndex];
+//     currentIndex = (currentIndex + 1) % texts.length;
+// }
+
+// 2초마다 텍스트 변경
+// setInterval(typeText, 2000); 
+
+// 커서 애니메이션 추가
+// const cursor = document.createElement('p');
+// cursor.className = 'cursor';
+// textElement.parentElement.appendChild(cursor);
+
+
